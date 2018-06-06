855405dd9f2f769d07b1c596c87f5c3a7be5a6bc
Brent Dearth <brent.dearth@gmail.com>
2018-06-06 09:39:54 -0600

    feat(observe): add mutation observer support in case DOM is being generated post onLoad

--- hide-gmail-addons-gutter.js
+++ hide-gmail-addons-gutter.js
@@ -1,6 +1,43 @@
 'use strict';
 
+function getGutterElem() {
+    return document.getElementsByClassName('nH bAw nn')[0];
+}
+
+function removeElem(elem) {
+    elem.parentNode.removeChild(elem);
+    console.info('hide-gmail-addons-gutter: removing ', elem);
+}
+
 window.onload = function() {
-    var gutter = document.getElementsByClassName('nH bAw nn')[0];
-    gutter.parentNode.removeChild(gutter);
+
+    var gutter = getGutterElem();
+
+    if (gutter) {
+
+        removeElem(gutter);
+
+    } else {
+
+        // Create an observer to look for known gutter elements
+        var observer = new MutationObserver(function(mutationsList) {
+            var gutter = getGutterElem();
+
+            // Remove the gutter and disconnect the observer if found
+            if (gutter) {
+                removeElem(gutter);
+                observer.disconnect();
+            }
+        });
+
+        // Spy on something reliable for mutation events
+        var mainContainer = document.getElementsByClassName('nH')[0];
+
+        // Start observing the target
+        observer.observe(mainContainer, {
+            childList: true
+        });
+
+    }
+
 };
